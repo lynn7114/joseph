dfd3902bf79e8937194849ba74e3e7f124835eb9
lynn7114 <154987575+lynn7114@users.noreply.github.com>
2025-04-05 15:45:08 +0900

    Update style.py

--- style.py
+++ style.py
@@ -17,3 +17,26 @@ def set_background(image_file):
         """,
         unsafe_allow_html=True
     )
+
+def set_custom_font(font_file, font_family):
+    with open(font_file, "rb") as f:
+        encoded_font = base64.b64encode(f.read()).decode()
+
+    st.markdown(
+        f"""
+        <style>
+        @font-face {{
+            font-family: '{font_family}';
+            src: url(data:font/ttf;base64,{encoded_font}) format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }}
+
+        html, body, .stApp {{
+            font-family: '{font_family}', sans-serif;
+            color: black; /* 글씨 색깔 검정 */
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
