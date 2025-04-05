31f82a48002656cfcdfc7cc0a5461eeea534428c
lynn7114 <154987575+lynn7114@users.noreply.github.com>
2025-04-05 15:42:38 +0900

    Update style.py

--- style.py
+++ style.py
@@ -1,14 +1,14 @@
+import base64
 import streamlit as st
 
 def set_background(image_file):
     with open(image_file, "rb") as f:
-        data = f.read()
-    encoded = f"data:image/png;base64,{data.encode('base64').decode()}"
+        encoded = base64.b64encode(f.read()).decode()
     st.markdown(
         f"""
         <style>
         .stApp {{
-            background-image: url("{encoded}");
+            background-image: url("data:image/png;base64,{encoded}");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
