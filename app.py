887628ed86fd677c354be24c918030c72ee3a79f
lynn7114 <154987575+lynn7114@users.noreply.github.com>
2025-04-07 21:19:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from separate import separate_problems, parse_primary_level_questions, extract_u
 from pptx import Presentation
 
 # API 키 로드
+openai.api_key=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
 api_key = os.getenv("OPENAI_API_KEY")
 st.write(f"API Key loaded: {api_key is not None}")
 
